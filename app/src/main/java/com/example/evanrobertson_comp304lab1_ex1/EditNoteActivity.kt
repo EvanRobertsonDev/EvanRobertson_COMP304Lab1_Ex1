12d08930b021834895870dafbbdd3967db9f0aa7
Evan Robertson <evan-robertson@outlook.com>
2024-09-28 23:03:20 -0400

    Updated Note Edit and Note Create to allow scrolling to prevent Save button from being inaccessable

--- app/src/main/java/com/example/evanrobertson_comp304lab1_ex1/EditNoteActivity.kt
+++ app/src/main/java/com/example/evanrobertson_comp304lab1_ex1/EditNoteActivity.kt
@@ -3,12 +3,15 @@ package com.example.evanrobertson_comp304lab1_ex1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
@@ -64,6 +67,9 @@ fun EditNoteLayout(note: Note, onSaveClick : (title : String, content : String) 
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
+            //Allow for scrolling
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
     ) {
         //Title field
         TextField(
